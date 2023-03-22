f4aa9c229312b06c19e18813fb2c750f9e683173
ramirosarasola <ramiro.sarasola@gmail.com>
2023-03-22 15:41:21 -0300

    agrego el proyecto con ORM

---
+++ src/main/java/com/example/proyectointegradororm/ProyectoIntegradorOrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectointegradororm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectointegradororm/ProyectoIntegradorOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectointegradororm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
